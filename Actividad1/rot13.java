04d8404f145f686b497cb08dc3d35bbdbcc92b5f
Marc Rubio <marc.rubio98@gmail.com>
2024-09-18 20:56:29 +0200

    Commit end class

--- Actividad1/rot13.java
+++ Actividad1/rot13.java
@@ -1,11 +1,27 @@
-public class rot13{
-    //Arrays de letras
-    private static final char[] minus = 'abcdefghijklmnñopqrstuvwxyz'.ToCharArray();
-    private static final char[] mayus = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'.ToCharArray();
-}
-public static String cifraROT13(String cadena){
-    StringBuilder resultado = new StringBuilder();
-    for (char c : cadena.ToArrayChar()){
-        if 
+public class Rot13{
+    // Arrays de letras
+    private static final char[] minus = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    private static final char[] mayus = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
+    // Cifrar ROT13
+    public static String cifraROT13(String cadena){
+        // Array para manipular
+        char[] aux = cadena.toCharArray();
+        for (int i = 0 ; i < cadena.length() ; i++){
+            char c = aux[i]; // Obtenemos valor actual
+            // En cualquier caso, desplazamos letra
+            if (Character.isLowerCase(c)){
+                aux[i] = DesplazarLetra(c, minus);
+            } else if (Character.isUpperCase(c)) {
+                aux[i] = DesplazarLetra(c, mayus);
+            }
+        }
+        return new String(aux);
+    }
+    private static char DesplazarLetra(char letra, char[] alfabeto){
+        for (int i = 0; i < alfabeto.length; i++) {
+            return alfabeto[(i + 13) % alfabeto.length];
+        }
+        return letra;
     }
 }
