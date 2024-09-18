83eb858ef904d00bfdcebbb243fcd3a64c662572
Marc Rubio <marc.rubio98@gmail.com>
2024-09-18 20:15:46 +0200

    Crear carpeta actividad y test subida

---
+++ Actividad1/rot13.java
@@ -0,0 +1,11 @@
+public class rot13{
+    //Arrays de letras
+    private static final char[] minus = 'abcdefghijklmnñopqrstuvwxyz'.ToCharArray();
+    private static final char[] mayus = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'.ToCharArray();
+}
+public static String cifraROT13(String cadena){
+    StringBuilder resultado = new StringBuilder();
+    for (char c : cadena.ToArrayChar()){
+        if 
+    }
+}
