c301404673f693a3fac27085cdbbdafcfde319cb
MarcRGitHubTIC <145706443+MarcRGitHubTIC@users.noreply.github.com>
2024-11-03 21:40:52 +0100

    Add files via upload

---
+++ Factory_06/src/iticbcn/xifratge/ClauNoSuportada.java
@@ -0,0 +1,8 @@
+package iticbcn.xifratge;
+
+public class ClauNoSuportada extends Exception {
+	private static final long serialVersionUID = 1L;
+	public ClauNoSuportada(String message) {
+        super(message);
+    }
+}
